33b3b63e0041cce3f1dba29339571dee85e2086c
Ivan Fuentes <jitsu.gokei@hotmail.com>
2018-04-19 10:59:06 +0200

    Index updated

--- Aeroport_Cercador/index.php
+++ Aeroport_Cercador/index.php
@@ -1,16 +1,33 @@
-<?php
-	echo "hola que tal ";
-	echo "<h1>HOLAAA WILLY</h1>";
-	$i = 100;
-	while ($i <= 200) {
+<meta charset="UTF-8">
+  <style type="text/css">
+      body {
+	     }
 
-		echo "While El numero es: " . $i++ . "<br>";
-	}
-	for ($i=100; $i<=200; $i++) {
-		echo "For El numero es: " . $i . "<br>";
-	}asd
-	$i=100;
-	do {
-		echo "Willy Dowhile: " . $i++ . "<br>";
-	} while ($i<=200);
-?>
+       input[type=submit] {
+			  }
+
+        input[type=submit]:hover {
+				}
+
+       input[type=text], input[type=password]{
+				}
+
+    </style>
+
+<center>
+
+//formulari
+
+<div class="contformhead">LOGIN INFO</div>
+<form action= "selectdb.php" method=GET class="contform">
+
+	Server IP<br><br>
+	<input type=text name=server value="server ip here" onfocus="if (this.value == 'server ip here') {this.value=''}"><br><br><br>
+	Username<br><br>
+	<input type=text name=username value="your user here" onfocus="if (this.value == 'your user here') {this.value=''}"><br><br><br>
+	Password<br><br>
+	<input type=password name=password value="your password here" onfocus="if (this.value == 'your password here') {this.value=''}"><br><br><br>
+
+	<table><tr><td><input type=submit value=LOGIN></td></form></td></tr></table>
+
+</center>
