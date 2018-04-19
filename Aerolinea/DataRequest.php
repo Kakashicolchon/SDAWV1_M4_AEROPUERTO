c16e5ff0f32c266b248803bee291b695e2f0a19d
Ivan Fuentes <jitsu.gokei@hotmail.com>
2018-04-19 10:22:53 +0200

    Changed name to DataRequest.php

---
+++ Aerolinea/DataRequest.php
@@ -0,0 +1,16 @@
+<?php
+	echo "hola que tal ";
+	echo "<h1>HOLAAA Aaron   </h1>";
+	$i = 100;
+	while ($i <= 200) {
+
+		echo "While El numero es: " . $i++ . "<br>";
+	}
+	for ($i=100; $i<=200; $i++) {
+		echo "For El numero es: " . $i . "<br>";
+	}
+	$i=100;
+	do {
+		echo "Willy Dowhile: " . $i++ . "<br>";
+	} while ($i<=200);
+?>
